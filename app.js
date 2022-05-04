844e868749b26b85ebb83341a5d32e920cdca521
austinbhan <austinbhan@outlook.com>
2022-05-03 17:06:44 -0700

    Created Save Meal Grid

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 // import functions and grab DOM elements
-
 const foodInput = document.getElementById('food-input');
-
 const removeLastItem = document.getElementById('remove-last-item');
 
+const listedIngredients = document.getElementById('listed-ingredients');
 
 // let state
-
 let totalIngredients = [];
 let meals = [];
 
-// set event listeners 
-
-// Add Ingredient Button
 foodInput.addEventListener('submit', (e) => {
     e.preventDefault();
     const foodData = new FormData(foodInput);
@@ -22,10 +17,26 @@ foodInput.addEventListener('submit', (e) => {
         measurement: foodData.get('measurement'),
     };
     totalIngredients.push(ingredients);  
+    appendIngredients();
 });
-// Remove Last Ingredient Button
+
+function appendIngredient(item) {
+    const li = document.createElement('li');
+    li.textContent = `${item.quantity} ${item.measurement} of ${item.ingredient}`;
+    return li;
+}
+
+function appendIngredients() {
+    listedIngredients.textContent = '';
+    for (let item of totalIngredients) {
+        const li = appendIngredient(item);
+        listedIngredients.appendChild(li);
+    }
+}
+
 removeLastItem.addEventListener('click', () => {
     totalIngredients.pop();
+    appendIngredients();
 });
 //
 
